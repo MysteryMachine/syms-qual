4681c7cc292a1c5abb82c4739258198967f9ba20
Sal Becker <physicsfu@gmail.com>
2017-10-13 17:33:53 -0400

    added day 2 dialogue and basic links

--- src/syms-qual/data/scenes.cljs
+++ src/syms-qual/data/scenes.cljs
@@ -1,16 +1,21 @@
 (ns syms-qual.data.scenes
   (:require [syms-qual.data.scenes.title-screen :as title-screen]
             [syms-qual.data.scenes.route-66.intro :as intro]
-            [syms-qual.data.scenes.route-66.options :as options]
-            [syms-qual.data.scenes.route-66.dates :as dates]
+            [syms-qual.data.scenes.route-66.options :as intro-options]
+            [syms-qual.data.scenes.route-66.dates :as intro-dates]
+
+            [syms-qual.data.scenes.anubis.cutscenes :as anubis-cutscenes]
+            [syms-qual.data.scenes.anubis.pharah :as anubis-pharah]
+            [syms-qual.data.scenes.anubis.genji :as anubis-genji]
+            [syms-qual.data.scenes.anubis.zarya :as anubis-zarya]
 
             [syms-qual.data.characters :as char]
             [syms-qual.data.bg :as bg]
             [carmen.data :as data]))
 
 (def structure
-  {:route-66 (merge intro/data options/data dates/data)
-   :anubis (merge)
+  {:route-66 (merge intro/data intro-options/data intro-dates/data)
+   :anubis (merge anubis-cutscenes/data anubis-pharah/data anubis-genji/data anubis-zarya/data)
    :eichenwalde (merge)
    :gibralter (merge)
    :dorado (merge)
