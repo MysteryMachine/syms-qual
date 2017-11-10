a09307c1fe0d208ee1bcd07cd52871e195ca8861
Sal Becker <physicsfu@gmail.com>
2017-11-09 19:14:53 -0500

    added new assets into codebase

--- src/syms-qual/data/bg.cljs
+++ src/syms-qual/data/bg.cljs
@@ -4,27 +4,45 @@
 (def structure
   ["anubis_cafe"
    "anubis_flashback"
-   "anubis_spawn"
    "anubis_genji"
-   "anubis_pharah"
+   "anubis_pharah_a"
    "anubis_pharah_b"
+   "anubis_spawn"
    "anubis_zarya"
 
+   "dorado_sombra_b"
+   "dorado_spawn"
+
+   "hollywood_lucio"
    "hollywood_objective"
    "hollywood_set"
-   "hollywood_theatre"
    "hollywood_soldier"
+   "hollywood_theatre"
 
-   "route_66_diner"
-   "route_66_street"
+   "junkertown_junkrat_a_1"
+   "junkertown_junkrat_a_2"
+
+   "kings_row_lucio"
+   "kings_row_street"
+   "kings_row_torb"
+
+   "lijang_junkrat_a"
+   "lijang_orca"
+
    "route_66_ana"
+   "route_66_diner"
+   "route_66_door"
    "route_66_hog"
    "route_66_reaper"
+   "route_66_street"
 
    "title_screen_bg"
 
+   "volskaya_apartment"
+   "volskaya_sombra_b"
    "volskaya_spawn"
-   "volskaya_zenyatta"])
+   "volskaya_zenyatta"
+   "volskaya_point_a"])
 
 (def options
   {:path "img/Backgrounds/"
